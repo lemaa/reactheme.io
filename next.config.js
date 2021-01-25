17635a7f7489f34b40a4769a3aae26643214be3f
amel <amel@3dosft.fr>
2021-01-25 17:50:08 +0100

    travis/gh-pages settings

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
-module.exports = {
-    distDir: 'build',
-    assetPrefix: '/reacttheme.io/',
+const withPlugins = require('next-compose-plugins');
+const optimizedImages = require('next-optimized-images');
+module.exports = withPlugins([
+  [optimizedImages, {
+    mozjpeg: {
+      quality: 80,
+    },
+    pngquant: {
+      speed: 3,
+      strip: true,
+      verbose: true,
+    },
+    imagesPublicPath: '/reacttheme.io',
+  }],
+  {
     basePath: '/reacttheme.io',
-
-}
+    assetPrefix: '/reacttheme.io/',
+   },
+]);
