4a968b24aea89932dfba7b6027694e28294f45db
amel <amel@3dosft.fr>
2021-02-12 14:50:20 +0100

    fix(components): fix missing theme in stories

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,25 @@
 
+import React from "react";
+import Head from "next/head";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { ThemeProvider } from "@material-ui/core/styles";
+import theme from "@Style/base/theme";
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
-}
+};
+
+export const decorators = [
+  (Story) => (
+    <>
+        <Head>
+            <title>stories</title>
+            <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+        </Head>
+        <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <Story />
+        </ThemeProvider>
+    </>
+  ),
+];
