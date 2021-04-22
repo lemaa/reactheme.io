a810c58c57436c151510f3b540968e1d5a4a7531
amel <amel@3dosft.fr>
2021-04-22 18:11:37 +0200

    fix(storybook): add AppsettingContext provider

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -5,6 +5,7 @@ import CssBaseline from "@material-ui/core/CssBaseline";
 import { ThemeProvider } from "@material-ui/core/styles";
 import theme from "@Style/base/theme";
 import { RouterContext } from 'next/dist/next-server/lib/router-context';
+import { AppSettingsProvider } from "@Context/index";
 import { I18nextProvider } from 'react-i18next';
 import i18n from './i18n';
 
@@ -14,21 +15,24 @@ export const parameters = {
 
 export const decorators = [
     (Story) => (
-        <I18nextProvider i18n={i18n}>
-            <RouterContext.Provider value={{
-                push: () => Promise.resolve(),
-                replace: () => Promise.resolve(),
-                prefetch: () => Promise.resolve()
-            }}>
-                <Head>
-                    <title>stories</title>
-                    <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
-                </Head>
-                <ThemeProvider theme={theme}>
-                    <CssBaseline />
-                    <Story />
-                </ThemeProvider>
-            </RouterContext.Provider>
-        </I18nextProvider >
+        <AppSettingsProvider>
+            <I18nextProvider i18n={i18n}>
+                <RouterContext.Provider value={{
+                    push: () => Promise.resolve(),
+                    replace: () => Promise.resolve(),
+                    prefetch: () => Promise.resolve()
+                }}>
+                    <Head>
+                        <title>stories </title>
+                        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+                    </Head>
+                    <ThemeProvider theme={theme}>
+                        <CssBaseline />
+                        <Story />
+                    </ThemeProvider>
+                </RouterContext.Provider>
+            </I18nextProvider >
+        </AppSettingsProvider>
+
     ),
 ];
