261382631226f78a1fee3df25d6f030abe6ee4bc
amel <amel@3dosft.fr>
2021-04-23 00:25:22 +0200

    chore(build script): test new script

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = withPlugins([
     {
-        assetPrefix: isProd ? 'https://cdn.statically.io/gh/lemaa/reactheme.io/gh-pages/' : '',
+        assetPrefix: isProd ? '' : '',
     },
     {
         i18n
