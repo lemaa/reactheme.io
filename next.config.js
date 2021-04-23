6b522c65436eb4b4dc1dc37c002f886e386b5e36
amel <amel@3dosft.fr>
2021-04-23 11:44:09 +0200

    chore(build script): test new script

--- next.config.js
+++ next.config.js
@@ -5,9 +5,6 @@ const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = withPlugins([
     {
-        assetPrefix: isProd ? '' : '',
-    },
-    {
         i18n
     },
 ]);
--- src/utils/Images.ts
+++ src/utils/Images.ts
@@ -1,7 +1,7 @@
 export const cdnImage = (name: string, folder = "/static/images") => {
     let srcPicture: string;
     if (process.env.NODE_ENV === "production") {
-        srcPicture = `https://lemaa.github.io/reactheme.io/${folder}/${name}`;
+        srcPicture = `${folder}/${name}`;
     } else {
         srcPicture = `${folder}/${name}`;
     }
