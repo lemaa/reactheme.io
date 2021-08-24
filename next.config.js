7c9c3194c01871ee70d52b36e89792fab457353d
amel <amel@3dosft.fr>
2021-08-24 11:36:09 +0200

    chore(config): confid bundle analyzer

--- next.config.js
+++ next.config.js
@@ -1,10 +1,14 @@
 const withPlugins = require("next-compose-plugins");
 const { i18n } = require('./next-i18next.config');
 
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+    enabled: process.env.ANALYZE === 'true'
+})
 const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = withPlugins([
     {
         i18n
     },
+    [withBundleAnalyzer]
 ]);
